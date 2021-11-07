82d0c6fa2b7566c90c20808eff305eea9eaa1b4e
Edney Bruno <edneybruno2@gmail.com>
2021-11-07 01:35:29 -0300

    Adicionado suporte à opção -V e opções inválidas

--- usuarios.sh
+++ usuarios.sh
@@ -10,13 +10,24 @@
 MENSAGEM_USO="
 Uso: $0 [-h]
     -h      Mostra esta tela de ajuda e sai
+    -V      Mostra a versão do programa e sai
 "
 
 # Tratamento das opções de linha de comando
-if test "$1" = "-h"; then
-    echo "$MENSAGEM_USO"
-    exit 0
-fi
+case $1 in
+    -h)
+        echo "$MENSAGEM_USO"
+        exit 0
+        ;;
+    -V)
+        echo "$0 Versão 3"
+        exit 0
+        ;;
+    *)
+        echo "Opção inválida: $1"
+        exit 1
+        ;;
+esac
 
 # Processamento
 cut -d : -f 1,5 /etc/passwd | tr : \\t

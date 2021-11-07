565cb6d21481977f1edc7e7653e883831917d67e
Edney Bruno <edneybruno2@gmail.com>
2021-11-07 01:18:41 -0300

    Adicionado suporte à opção -h

--- usuarios.sh
+++ usuarios.sh
@@ -7,5 +7,16 @@
 #
 # Edney, Novembro de 2021
 
+MENSAGEM_USO="
+Uso: $0 [-h]
+    -h      Mostra esta tela de ajuda e sai
+"
+
+# Tratamento das opções de linha de comando
+if test "$1" = "-h"; then
+    echo "$MENSAGEM_USO"
+    exit 0
+fi
+
 # Processamento
 cut -d : -f 1,5 /etc/passwd | tr : \\t

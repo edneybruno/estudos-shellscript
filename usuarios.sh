820655307a636f71dd37a0cd721767747c3f61db
Edney Bruno <edneybruno2@gmail.com>
2021-11-07 01:43:01 -0300

    Arrumado bug quando não tem opções, basename no nome do programa, -V extraindo direto de uma variável, adicionadas opções --help e --version

--- usuarios.sh
+++ usuarios.sh
@@ -6,26 +6,28 @@
 # Obs.: Lê dados do arquivo /etc/passwd
 #
 # Edney, Novembro de 2021
-
+VERSAO=4
 MENSAGEM_USO="
-Uso: $0 [-h]
-    -h      Mostra esta tela de ajuda e sai
-    -V      Mostra a versão do programa e sai
+Uso: $(basename "$0") [-h | -V]
+    -h, --help      Mostra esta tela de ajuda e sai
+    -V, --version   Mostra a versão do programa e sai
 "
 
 # Tratamento das opções de linha de comando
 case $1 in
-    -h)
+    -h | --help)
         echo "$MENSAGEM_USO"
         exit 0
         ;;
-    -V)
-        echo "$0 Versão 3"
+    -V | --version)
+        echo "$(basename "$0") Versão $VERSAO"
         exit 0
         ;;
     *)
-        echo "Opção inválida: $1"
-        exit 1
+        if test -n "$1"; then
+            echo "Opção inválida: $1"
+            exit 1
+        fi
         ;;
 esac
 
